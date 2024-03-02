860939ce8b3f055296e8212d16ffb224ed7a8088
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-03-02 21:03:48 +0200

    Initialize & Select variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const playBtn = document.getElementById('play');
+const scoreEl = document.getElementById('high-score');
+const levelEl = document.getElementById('level');
+
+const pattern = [];
+let currentLevel;
+const tileChoices = ['green', 'red', 'blue', 'yellow'];
